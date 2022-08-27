1d00b42760e3a17556e298d60f02e29c6d2becff
Никита Полянский <nikpolanskiy17@gmail.com>
2022-08-27 10:57:29 +0500

    removed trailing whitespases

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,22 +1,21 @@
-from rest_framework import permissions 
- 
- 
-class IsOwnerOrReadOnly(permissions.BasePermission): 
-    def has_permission(self, request, view): 
-        return ( 
-            request.method in permissions.SAFE_METHODS 
-            or request.user.is_authenticated 
-        ) 
- 
-    def has_object_permission(self, request, view, obj): 
-        return ( 
-            request.method in permissions.SAFE_METHODS 
-            or ( 
-                request.user.is_authenticated 
-                and ( 
-                    obj.author == request.user 
-                    or request.user.is_superuser 
-                ) 
-            ) 
-        ) 
- 
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and (
+                    obj.author == request.user
+                    or request.user.is_superuser
+                )
+            )
+        )
